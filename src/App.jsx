dec49fccb85500ae811e8f504a0f3e8a7e8c62c4
Jude Tochy <judetochyokoye@gmail.com>
2023-01-18 18:50:13 +0100

    added page loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
 import Footer from './Components/Footer/Footer';
-
+import DotLoader from "react-spinners/DotLoader";
 
 
 function App() {
   const [isClicked, setClick ] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(()=>{
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  })
   const mobileMenuHandler =()=> setClick(!isClicked);
   return (
     <div className={`App ${isClicked ? `shorten` : '' } `}>
-      <Header isClicked={isClicked} setClick={mobileMenuHandler} />
-      <Main />
-      <Footer />
+      {
+          isLoading ?
+          <div className="loader">
+            <DotLoader color={'hsl(231, 69%, 60%)'} size={50} />
+          </div>
+          : 
+          <>
+            <Header isClicked={isClicked} setClick={mobileMenuHandler} />
+            <Main/>
+            <Footer/>
+          </>
+        }
     </div>
   )
 }
