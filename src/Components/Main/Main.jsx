76411aadf184a4f08381c9112bfbe89f06a71650
Jude Tochy <judetochyokoye@gmail.com>
2023-03-13 10:51:01 +0100

    edited animation

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -9,7 +9,6 @@ import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 
-
 const heroVariants = {
   initial: {
     opacity: 0,
@@ -17,49 +16,48 @@ const heroVariants = {
   final: {
     opacity: 1,
     transition: {
-      delay: 1,
       duration: 1,
       staggerChildren: 1,
-    }
-  }
-}
+    },
+  },
+};
 const heroChildVariants = {
   left: {
-    x: '-25%',
+    x: "-25%",
   },
   right: {
-    x: '25%',
+    x: "25%",
   },
   final: {
     x: 0,
     transition: {
       duration: 1,
       staggerChildren: 1,
-    }
+    },
   },
   btnLeft: {
-    x: '-100vw',
+    x: "-100vw",
   },
   btnRight: {
-    x: '100vw',
+    x: "100vw",
   },
-  btnFinal : {
+  btnFinal: {
     x: 0,
-    transition:{
+    transition: {
       delay: 1,
       duration: 2,
-    }
+    },
   },
-  btnFinal2 : {
+  btnFinal2: {
     x: 0,
-    transition:{
+    transition: {
       delay: 2,
       duration: 2,
-      type : 'spring',
-      stiffness : 300
-    }
-  }
-}
+      type: "spring",
+      stiffness: 300,
+    },
+  },
+};
 
 const Main = () => {
   const [features, setFeatures] = useState(featuresData);
@@ -74,8 +72,8 @@ const Main = () => {
   }, [switchNo]);
 
   // for the form
-  const [email, setEmail] = useState("")
-  const [errorMsg, setErrorMsg]= useState(null);
+  const [email, setEmail] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
   const [isValid, setIsValid] = useState(false);
   const [isSubmitting, setSubmit] = useState(false);
 
@@ -109,17 +107,19 @@ const Main = () => {
   return (
     <main>
       <div className="main" ref={mainRef}>
-        <motion.div className="hero"
-        variants={heroVariants}
-        initial= 'initial'
-        viewport={{ once: true }}
-        animate = 'final'
+        <motion.div
+          className="hero"
+          variants={heroVariants}
+          initial="initial"
+          viewport={{ once: true }}
+          whileInView="final"
         >
-          <motion.div className="hero-img"
-          variants={heroChildVariants}
-          initial='left'
-
-          whileInView='final'
+          <motion.div
+            className="hero-img"
+            variants={heroChildVariants}
+            initial="left"
+            viewport={{ once: true }}
+            whileInView="final"
           >
             <img
               src="/assets/illustration-hero.svg"
@@ -127,15 +127,17 @@ const Main = () => {
               className=""
             />
           </motion.div>
-          <motion.div className="hero-text"
-          variants={heroChildVariants}
-          initial= 'right'
-          viewport={{ once: true }}
-          whileInView='final'
+          <motion.div
+            className="hero-text"
+            variants={heroChildVariants}
+            initial="right"
+            viewport={{ once: true }}
+            whileInView="final"
           >
             <div className="text">
-              <h1>A Simple Bookmark
-              <strong>
+              <h1>
+                A Simple Bookmark
+                <strong>
                   {text}
                   <Cursor cursorColor="hsl(0, 94%, 66%)" />
                 </strong>
@@ -146,17 +148,19 @@ const Main = () => {
                 instantly. Try it for free.
               </p>
               <div className="hero-btn">
-                <motion.div className="btn1"
-                variants={heroChildVariants}
-                initial= 'btnLeft'
-                animate = 'btnFinal'
+                <motion.div
+                  className="btn1"
+                  variants={heroChildVariants}
+                  initial="btnLeft"
+                  animate="btnFinal"
                 >
                   <MediumButton name="Get it on Chrome" />
                 </motion.div>
-                <motion.div className="btn2"
-                variants={heroChildVariants}
-                initial= 'btnRight'
-                animate = 'btnFinal2'
+                <motion.div
+                  className="btn2"
+                  variants={heroChildVariants}
+                  initial="btnRight"
+                  animate="btnFinal2"
                 >
                   <LiteButton name="Get it on Firefox" />
                 </motion.div>
@@ -164,32 +168,48 @@ const Main = () => {
             </div>
           </motion.div>
         </motion.div>
-        <motion.div className="features"
-        id="feature"
-        variants={heroVariants}
-        initial= 'initial'
-        animate = 'final'
+        <motion.div
+          className="features"
+          id="feature"
+          variants={heroVariants}
+          initial="initial"
+          animate="final"
         >
           <div className="in-features">
             <h2>Features</h2>
             <p className="fp">
               Our aim is to make it quick and easy for you to access your
-              favourite websites. Your bookmarks sync between your devices so you
-              can access them on
-              the go.
+              favourite websites. Your bookmarks sync between your devices so
+              you can access them on the go.
             </p>
           </div>
           <div className="switch">
-            <div className={`tab1 ${switchNo === 0 ? `active` : ''}`} style={{ marginLeft: "0px" }}>
-              <button className={switchNo === 0 ? `activebtn` : ''} onClick={() => setSwitch(0)}>
+            <div
+              className={`tab1 ${switchNo === 0 ? `active` : ""}`}
+              style={{ marginLeft: "0px" }}
+            >
+              <button
+                className={switchNo === 0 ? `activebtn` : ""}
+                onClick={() => setSwitch(0)}
+              >
                 Simple Bookmarking
               </button>
             </div>
-            <div className={`tab2 ${switchNo === 1 ? `active` : ''}`}>
-              <button className={switchNo === 1 ? `activebtn` : ''} onClick={() => setSwitch(1)}>Speedy Searching</button>
+            <div className={`tab2 ${switchNo === 1 ? `active` : ""}`}>
+              <button
+                className={switchNo === 1 ? `activebtn` : ""}
+                onClick={() => setSwitch(1)}
+              >
+                Speedy Searching
+              </button>
             </div>
-            <div className={`tab3 ${switchNo === 2 ? `active` : ''}`}>
-              <button className={switchNo === 2 ? `activebtn` : ''} onClick={() => setSwitch(2)}>Easy Sharing</button>
+            <div className={`tab3 ${switchNo === 2 ? `active` : ""}`}>
+              <button
+                className={switchNo === 2 ? `activebtn` : ""}
+                onClick={() => setSwitch(2)}
+              >
+                Easy Sharing
+              </button>
             </div>
           </div>
           <div className="switch-comp">
@@ -204,10 +224,11 @@ const Main = () => {
             </div>
           </div>
         </motion.div>
-        <motion.div className="downloads"
-        variants={heroVariants}
-        initial= 'initial'
-        animate = 'final'
+        <motion.div
+          className="downloads"
+          variants={heroVariants}
+          initial="initial"
+          animate="final"
         >
           <h2>Download the extension</h2>
           <p className="fp">
@@ -247,11 +268,18 @@ const Main = () => {
                   type="text"
                   name=""
                   placeholder="Enter your email address"
-                  className={isValid ? 'red' : 'green'}
+                  className={isValid ? "red" : "green"}
                   onChange={handleMail}
                   onKeyUp={handleMail}
                 />
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className={errorMsg === "Whoops! This Email Address" ? `` : 'hid'}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  className={
+                    errorMsg === "Whoops! This Email Address" ? `` : "hid"
+                  }
+                >
                   <g fill="none" fillRule="evenodd">
                     <circle cx="10" cy="10" r="10" fill="#FA5959" />
                     <g fill="#FFF" transform="translate(9 5)">
@@ -261,14 +289,14 @@ const Main = () => {
                   </g>
                 </svg>
                 <div className="error mobile">
-                <p className="">{ errorMsg }</p>
-              </div>
+                  <p className="">{errorMsg}</p>
+                </div>
                 <div className="form-btn">
-                  <LoginButton name={isSubmitting ? 'Sending': 'Contact Us'} />
+                  <LoginButton name={isSubmitting ? "Sending" : "Contact Us"} />
                 </div>
               </div>
               <div className="error desktop">
-                <p className="">{ errorMsg }</p>
+                <p className="">{errorMsg}</p>
               </div>
             </form>
           </div>
